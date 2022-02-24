b1006ff2fca7e99aa0a0110b76342c1877157d4e
Prophet-Bestman <bestiproph@gmail.com>
2022-02-24 05:10:13 +0100

    Added base typography styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Epilogue", "sans-serif"],
+      },
+      colors: {
+        accent: "#EF6D58",
+        "dark-gray": "rgba(57, 20, 0, 0.64)",
+        "light-gray": "rgba(255, 255, 255, 0.64)",
+      },
+    },
   },
   plugins: [],
 };
