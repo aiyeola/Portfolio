4a79b158077bc8870807a0667d581aceda0c6be9
Prophet-Bestman <bestiproph@gmail.com>
2022-03-02 08:19:22 +0100

    Fixing skill circle

--- components/SkillCircle.jsx
+++ components/SkillCircle.jsx
@@ -4,12 +4,12 @@ import React from "react";
 const SkillCircle = ({ img }) => {
   return (
     <div className="bg-white rounded-full py-8 px-9 drop-shadow-2xl shadow-md items-center inline-grid hover:-translate-y-6 hover:scale-110 duration-500 ease-out">
-      <img
+      <Image
         width="100%"
         height="100%"
         src={img}
         alt=""
-        className=" shadow-lg mx-auto my-auto"
+        className="h-full w-full shadow-lg mx-auto my-auto"
       />
     </div>
   );
